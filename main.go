8f5eb2bb5708e1e673fdb5060cd73489a8ae4e90
Kai-Cheok <kai.cheok@pracedo.com>
2022-03-29 14:24:59 +0100

    added styling

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", api.HomeHandler)
 	mux.HandleFunc("/artists", api.ArtistHandler)
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
 	if err := http.ListenAndServe(":8000", mux); err != nil {
 		log.Fatal(err)
 	}
