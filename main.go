dcdd81752c66984ce5229a1698713f62893a8479
Kai-Cheok <kai.cheok@pracedo.com>
2022-04-02 18:49:48 +0100

    styling of home and artist page

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 	mux.HandleFunc("/", api.HomeHandler)
 	mux.HandleFunc("/artists", api.ArtistHandler)
 	mux.HandleFunc("/locations", api.LocationHandler)
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
 	if err := http.ListenAndServe(":8000", mux); err != nil {
 		log.Fatal(err)
 	}
